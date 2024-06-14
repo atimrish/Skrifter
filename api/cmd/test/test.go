925563ee5a7af465c1c2020dfa39182cbbbb1214
atimrish <atimrish11@gmail.com>
2024-06-14 06:18:03 +0400

    last

--- api/cmd/test/test.go
+++ api/cmd/test/test.go
@@ -1,32 +1,13 @@
 package test
 
 import (
-	"log"
-	"net/smtp"
+	"api/app/actions"
+	"api/app/models"
 )
 
 func Test() {
+	db := actions.GetDb()
+	defer actions.CloseDb(db)
 
-	from := "strcty11@gmail.com"
-
-	auth := smtp.PlainAuth(
-		"",
-		from,
-		"s6^%cf-<",
-		"smtp.gmail.com",
-	)
-
-	to := []string{"atimrish11@gmail.com"}
-	msg := []byte("To:from\r\n test message\r\n")
-	err := smtp.SendMail(
-		"smtp.gmail.com:587",
-			auth,
-			from,
-			to,
-			msg,
-	)
-
-	if err != nil {
-		log.Fatal(err)
-	}
+	db.AutoMigrate(&models.Feedback{})
 }
